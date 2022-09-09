016f1ef44a61f0a691396761aeb9d5106cd89fbc
Muukii <muukii.app@gmail.com>
2022-09-10 00:31:28 +0900

    Initial Commit

---
+++ Sources/WrapLayout/WrapLayout.swift
@@ -0,0 +1,6 @@
+public struct WrapLayout {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/WrapLayoutTests/WrapLayoutTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WrapLayout
+
+final class WrapLayoutTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WrapLayout().text, "Hello, World!")
+    }
+}
