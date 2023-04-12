54c8711e632b1331bd835c57c5b5c92ec463b31d
Muukii <muukii.app@gmail.com>
2023-04-12 18:28:17 +0900

    Add test lib

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/sampettersson/Placement", from: "1.4.1")
+    .package(url: "https://github.com/sampettersson/Placement", from: "1.4.1"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.1.0"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -31,7 +32,7 @@ let package = Package(
     ),
     .testTarget(
       name: "WrapLayoutTests",
-      dependencies: ["WrapLayout"]
+      dependencies: ["WrapLayout", .product(name: "SnapshotTesting", package: "swift-snapshot-testing")]
     ),
   ]
 )
